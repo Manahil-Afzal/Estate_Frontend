7fa55415adc7e1dcec47576b0b5bc9de205cc723
HP <manahilafzal721@gmail.com>
2025-08-28 07:40:00 -0700

    /

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -26,7 +26,7 @@ export default function SignUp() {
         password: formData.password,
       };
 
-      const res = await fetch(`${API_BASE_URL}/auth/signup`,  {
+      const res = await fetch(`${API_BASE_URL}/auth/register`,  {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
